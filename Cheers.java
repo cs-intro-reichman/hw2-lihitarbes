60c8118bc624adcf23c0adb2a0c4dad0d287ac73
lihitarbes <mikilihi@gmail.com>
2024-11-19 14:43:48 +0200

    Cheers

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,32 @@
 // Prints a crowd cheering output.
+
+
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	   String cheers = args[0];
+            int numofcheers = Integer.parseInt(args[1]) ;
+            int i = 0;
+            String capital = cheers.toUpperCase();
+            while (i < cheers.length())
+        { 
+                if( capital.charAt(i) == 'A' || capital.charAt(i) == 'E'|| capital.charAt(i) == 'F' || capital.charAt(i) == 'H' || capital.charAt(i) == 'I' || capital.charAt(i) == 'L' || capital.charAt(i) == 'M'|| capital.charAt(i) == 'N' || capital.charAt(i) == 'O' || capital.charAt(i) == 'R'  ){
+                        System.out.println("Give Me an " + capital.charAt(i) + ":" + " " + capital.charAt(i) + "!");
+                }
+                else {
+                        System.out.println("Give Me a " + capital.charAt(i) + ":" + " " + capital.charAt(i) + "!") ;      
+                }
+                i++; 
+           
+        }
+        System.out.println("What does that spell?");
+        System.out.println(capital + "!!!");
+        
+        for( int j = 0; j< numofcheers; j++)
+        {
+                System.out.println(capital + "!!!");
+             
         }
 }
+
+        }
+
